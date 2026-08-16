2df922466136ed8ac484c24f616f499ff6d9b40f
enrique <kike.humberto.sepulveda@gmail.com>
2026-08-15 20:31:23 -0400

    problema resulto, actividad 1 progra

--- actividad/main.py
+++ actividad/main.py
@@ -1,8 +1,18 @@
 import os
+from producto import Producto
 
 
 def cargar_productos(ruta: str) -> list:
     productos = []
+    
+    with open(ruta, "r", encoding = "ascii") as archivo:
+        lineas = archivo.readlines()
+    datos = [linea.strip("\n") for linea in lineas]
+    
+    for product in datos:
+        separar_datos = product.split(",")
+        productos.append(Producto(str(separar_datos[0]), int(separar_datos[1]), int(separar_datos[2])))
+        
     return productos
 
 
@@ -15,8 +25,8 @@ if __name__ == '__main__':
     ruta = os.path.join('data', 'menu.txt')
     productos = cargar_productos(ruta)
     print('=== DCCafeteria ===')
-    for producto in productos:
-        print(producto.descripcion())
+    for product in productos:
+        print(product.descripcion())
     print(f'Productos registrados: {Producto.total_productos}')
     simular_ventas(productos)
     simular_ventas(productos)
--- actividad/producto.py
+++ actividad/producto.py
@@ -1,9 +1,18 @@
 class Producto:
-    def __init__(self, nombre: str, precio: int, stock: int) -> None:
-        pass
+    total_productos = 0
+    def __init__(self, nombre: str, precio: int, stock: int):
+        self.nombre = nombre
+        self.precio = precio
+        self._stock = stock
+        Producto.total_productos += 1
 
     def descripcion(self) -> str:
         return f'{self.nombre}: ${self.precio} (stock: {self._stock})'
 
-    def vender(self) -> None:
-        pass
+    def vender(self) -> str:
+        if self._stock == 0:
+            print(f"No queda stock de {self.nombre}")
+        
+        if self._stock >= 1:
+            self._stock -= 1
+            print(f"Vendido: {self.nombre}. Stock restante: {self._stock}")
